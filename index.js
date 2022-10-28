8aa4b31a2e7b08a055f86a64f9b827fe4a5e6372
AEFLR <farelcahyo95@gmail.com>
2022-10-28 19:27:44 +0700

    Bismillah

---
+++ index.js
@@ -0,0 +1,59 @@
+function total(buku, diskon, pajak, stock, pemesanan, kredit){
+    let result = 0;
+
+    console.log(buku.judul);
+    for(let index = 0; index < stock; index++){
+        if(buku && buku.harga && index < pemesanan){
+            const judul = buku.judul;
+            const harga = buku.harga;
+            const totDiskon = harga * (diskon/100);
+            const hrgDiskon = harga - totDiskon;
+            const totPajak = hrgDiskon * (pajak/100);
+            const hrgPajak = hrgDiskon + totPajak;
+            result = result + hrgPajak;
+    
+            // console.group();
+            // console.log(judul);
+            // console.log('--------------------');
+            // console.log('Harga Awal : ',harga);
+            // console.log('Diskon : ',diskon,'%');
+            // console.log('Harga Diskon : ',hrgDiskon);
+            // console.log('Pajak : ',pajak,'%');
+            // console.log('Harga Total : Rp',hrgPajak,',-');
+            // console.log();
+            // console.log();
+            // console.groupEnd();
+
+            console.group();
+            console.log('Total ', index + 1, ' Buku Sebesar Rp', result, ',-');
+            console.groupEnd();
+        }else{
+            console.log('Stok Habis')
+        }
+    }
+
+    console.group();
+    console.log('----------------------');
+    console.log('Total Harga yang Harus Dibayar : Rp',result,',-');
+    console.log();
+    console.log();
+    console.log('Dikredit ', kredit, 'Bulan');
+    console.groupEnd();
+
+    let Tempo = result/kredit;
+    let bayar = [];
+    for(let index=0; index < kredit; index++){
+        bayar.push(
+            {
+                Angsuran_ke: index + 1,
+                Rp: Tempo
+            }
+        )
+    }
+
+    console.log(Array.from(bayar));
+    return result;
+
+} 
+
+total({judul: "Manga One Piece", harga: 20000000}, 15, 5, 3, 3, 12);
