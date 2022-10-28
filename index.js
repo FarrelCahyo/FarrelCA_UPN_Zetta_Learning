36c14585f9565c7f514a3394523938c77bb8184a
AEFLR <farelcahyo95@gmail.com>
2022-10-28 18:52:04 +0700

    Bismillah

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-function total(buku, diskon, pajak){
+function total(buku, diskon, pajak, stock, pemesanan){
     let result = 0;
-    if(buku && buku.harga){
-        const judul = buku.judul;
-        const harga = buku.harga;
-        const totDiskon = harga * (diskon/100);
-        const hrgDiskon = harga - totDiskon;
-        const totPajak = hrgDiskon * (pajak/100);
-        const hrgPajak = hrgDiskon + totPajak;
-        result += hrgPajak;
 
-        console.group();
-        console.log(judul);
-        console.log('--------------------');
-        console.log('Harga Awal : ',harga);
-        console.log('Diskon : ',diskon,'%');
-        console.log('Harga Diskon : ',hrgDiskon);
-        console.log('Pajak : ',pajak,'%');
-        console.log('Harga Total : Rp',hrgPajak,',-');
-        console.log();
-        console.log();
-        console.groupEnd();
+    console.log(buku.judul);
+    for(let index = 0; index < stock; index++){
+        if(buku && buku.harga && index < pemesanan){
+            const judul = buku.judul;
+            const harga = buku.harga;
+            const totDiskon = harga * (diskon/100);
+            const hrgDiskon = harga - totDiskon;
+            const totPajak = hrgDiskon * (pajak/100);
+            const hrgPajak = hrgDiskon + totPajak;
+            result = result + hrgPajak;
+    
+            // console.group();
+            // console.log(judul);
+            // console.log('--------------------');
+            // console.log('Harga Awal : ',harga);
+            // console.log('Diskon : ',diskon,'%');
+            // console.log('Harga Diskon : ',hrgDiskon);
+            // console.log('Pajak : ',pajak,'%');
+            // console.log('Harga Total : Rp',hrgPajak,',-');
+            // console.log();
+            // console.log();
+            // console.groupEnd();
+
+            console.group();
+            console.log('Total ', index + 1, ' Buku Sebesar Rp', result, ',-');
+            console.groupEnd();
+        }else{
+            console.log('Stok Habis')
+        }
     }
+
+    console.group();
+    console.log('----------------------');
+    console.log('Total Harga yang Harus Dibayar : Rp',result,',-');
+    console.log();
+    console.groupEnd();
+
 } 
 
-total({judul: "Manga One Piece", harga: 20000000}, 15, 5);
-total({judul: "Manga Naruto", harga: 10000000}, 10, 3);
-total({judul: "Manga Gintama", harga: 8000000}, 8, 2);
+total({judul: "Manga One Piece", harga: 20000000}, 15, 5, 3, 3);
